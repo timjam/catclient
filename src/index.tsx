f46a6ec1507372e03ed9b54a4a5831e709c8d63f
Timo Mätäsaho <timo.matasaho@gmail.com>
2020-04-21 10:57:12 +0300

    Change root to use breed view

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import BreedsView from './views/BreedsView';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BreedsView />
   </React.StrictMode>,
   document.getElementById('root')
 );
