73db508a41b95958c54e4acd8ff0a38a9de9f15c
Timo Mätäsaho <timo.matasaho@gmail.com>
2020-04-22 19:16:42 +0300

    Change components to use bootstrap

--- src/components/PropertySelector.tsx
+++ src/components/PropertySelector.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { PropertySelectorProps } from '../types/GenericComponents';
+import { Form } from 'react-bootstrap';
 
 const PropertySelector = (props: PropertySelectorProps) => {
 
   const uniqueLabels = Array.from(new Set(props.labels));
 
   return (
-    <form>
-      <label>
-        {`Filter ${props.property}`}
-        <select value={props.value} onChange={event => props.handleOnChange(event)}>
-          {uniqueLabels.map((label, index) => <option key={index} value={label}>{label}</option>)}
-        </select>
-      </label>
-    </form>
+    // <form>
+    //   <label>
+    //     {`Filter ${props.property}`}
+    //     <select value={props.value} onChange={event => props.handleOnChange(event)}>
+    //       {uniqueLabels.map((label, index) => <option key={index} value={label}>{label}</option>)}
+    //     </select>
+    //   </label>
+    // </form>
+    <Form.Group>
+      <Form.Label>{`Filter ${props.property}`}</Form.Label>
+      <Form.Control as="select" value={props.value} onChange={event => props.handleOnChange(event)}>
+        {uniqueLabels.map((label, index) => <option key={index} value={label}>{label}</option>)}
+      </Form.Control>
+    </Form.Group>
   );
 };
 
--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../styles/SearchBox.css';
 import { SearchBoxProps } from '../types/GenericComponents';
+import { Form } from 'react-bootstrap';
 
 const SearchBox = (props: SearchBoxProps) => {
   return (
-    <form>
-      <label>
-        Name
-        <textarea className="SearchBox" value={props.name} onChange={event => props.handleOnChange(event)} />
-      </label>
-    </form>
+    <Form.Group controlId="formGroupSearchBox">
+      <Form.Label>Search by name</Form.Label>
+      {/* There seems to be some problem with FormControl event types and I don't have time to investigate it 
+          and create a correct type for event. That's why it is set to any. -Timo */}
+      <Form.Control type="input" value={props.name} onChange={(event: any) => props.handleOnChange(event)} />
+    </Form.Group>
   );
 };
 
