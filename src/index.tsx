5c2929894e5aa15fc28eb7eda60ccd6f5f759bde
Timo Mätäsaho <timo.matasaho@gmail.com>
2020-04-22 19:17:33 +0300

    Add styling and cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
 import BreedsView from './views/BreedsView';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BreedsView />
+      <BreedsView />
   </React.StrictMode>,
   document.getElementById('root')
 );
